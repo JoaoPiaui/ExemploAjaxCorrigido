77410d317a020a68c65ba1ff8fcd8ccc5ec3a390
Alunos <gersonsbeltrame@gmail.com>
2019-08-16 11:16:47 -0300

    CRUD da tabela vendas

--- Repository/BancoDados/Estrutura.sql
+++ Repository/BancoDados/Estrutura.sql
@@ -23,5 +23,16 @@ id INT PRIMARY KEY IDENTITY(1,1),
 id_venda INT, FOREIGN KEY (id_venda) REFERENCES vendas(id),
 nome VARCHAR (100),
 quantidade INT,
-valor DECIMAL (8,2)
+valor DECIMAL (8,2),
+registro_ativo BIT
 );
+
+INSERT INTO vendas(id_cliente, descricao, registro_ativo) VALUES
+(1, 'Oi', 1),
+(2, 'Tchau', 1);
+
+INSERT INTO produtos(id_venda, quantidade, valor, registro_ativo, nome) VALUES
+(1, 20, 29.99, 1, 'Mercedes Benz'),
+(1, 17, 329.99, 1, 'Filhote de Jacaré'),
+(1, 8001, 12.13, 1, 'Juliet'),
+(2, 20, 10000.00, 1, 'Pão de Queijo');
